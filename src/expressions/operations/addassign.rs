fba7b2e199792392ebada0c421a52eba38531e84
nbliznashki <nikifor.c.bliznashki@gmail.com>
2020-09-23 14:18:02 +0200

    rework to support slices

--- src/expressions/operations/addassign.rs
+++ src/expressions/operations/addassign.rs
@@ -1,45 +1,151 @@
 use crate::bitmap::Bitmap;
-use paste::paste;
-
-use std::ops::AddAssign;
-use std::ops::Deref;
-use std::ops::DerefMut;
-
 use crate::*;
 
-#[allow(dead_code)]
+use std::ops::AddAssign;
+
+use paste::paste;
+
 const OP: &str = "+=";
 
+//Load operation for the 4 combinations of output/input equal to Vec or Slice
 macro_rules! operation_load {
     ($dict:ident; $(($tl:ty, $tr:ty))+) => ($(
-            let signature=sig![OP; Vec<$tl>, Vec<$tr>];
-            let op=Operation{
-                f:  paste!{[<addassign_vec $tl:lower _ vec $tr:lower>]},
-                output_type: std::any::TypeId::of::<Vec<$tl>>(),
-                output_typename: std::any::type_name::<Vec<$tl>>().to_string()
-            };
+            {
+                //Vec Vec
+                type V1=Vec<$tl>;
+                type V2=Vec<$tr>;
+                let signature=sig![OP; V1, V2];
+                let op=Operation{
+                    f:  paste!{[<addassign_vec $tl:lower _ vec $tr:lower>]},
+                    output_type: std::any::TypeId::of::<V1>(),
+                    output_typename: std::any::type_name::<V1>().to_string()
+                };
             $dict.insert(signature, op);
-            let signature=sig![OP; Box<[$tl]>, Box<[$tr]>];
-            let op=Operation{
-                f:  paste!{[<addassign_boxslice $tl:lower _ boxslice $tr:lower>]},
-                output_type: std::any::TypeId::of::<Box<[$tl]>>(),
-                output_typename: std::any::type_name::<Box<[$tl]>>().to_string()
-            };
-            $dict.insert(signature, op);
+            }
+            {
+                //Slice Slice
+                type V1=[$tl];
+                type V2=[$tr];
+                let signature=sig![OP; V1, V2];
+                let op=Operation{
+                    f:  paste!{[<addassign_slice $tl:lower _ slice $tr:lower>]},
+                    output_type: std::any::TypeId::of::<V1>(),
+                    output_typename: std::any::type_name::<V1>().to_string()
+                };
+                $dict.insert(signature, op);
+            }
+            {
+                //Vec Slice
+                type V1=Vec<$tl>;
+                type V2=[$tr];
+                let signature=sig![OP; V1, V2];
+                let op=Operation{
+                    f:  paste!{[<addassign_vec $tl:lower _ slice $tr:lower>]},
+                    output_type: std::any::TypeId::of::<V1>(),
+                    output_typename: std::any::type_name::<V1>().to_string()
+                };
+                $dict.insert(signature, op);
+            }
+            {
+                //Slice Vec
+                type V1=[$tl];
+                type V2=Vec<$tr>;
+                let signature=sig![OP; V1, V2];
+                let op=Operation{
+                    f:  paste!{[<addassign_slice $tl:lower _ vec $tr:lower>]},
+                    output_type: std::any::TypeId::of::<V1>(),
+                    output_typename: std::any::type_name::<V1>().to_string()
+                };
+                $dict.insert(signature, op);
+            }
     )+)
 }
-
+//Define operation for the 4 combinations of output/input equal to Vec or Slice
+//Downcast input/output and then call the generic function addcolumn_c
 macro_rules! operation_impl {
     ($(($tl:ty, $tr:ty))+) => ($(
-        #[allow(dead_code)]
         paste!   {
             fn [<addassign_vec $tl:lower _ vec $tr:lower>](output: &mut ColumnWrapper, input: Vec<InputTypes>)
             {
-                addassign_c::<Vec<$tl>, Vec<$tr>, $tl, $tr>(output, input);
+                //Vec Vec
+                type V1=Vec<$tl>;
+                type V2=Vec<$tr>;
+                let (data_output, index_output, bitmap_output) = output.all_mut::<V1>();
+                let (data_input, index_input, bitmap_input) = match &input[0] {
+                    InputTypes::Ref(a) => (
+                        a.downcast_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                    InputTypes::Owned(a) => (
+                        a.downcast_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                };
+                addassign_c(data_output, index_output,bitmap_output, data_input, index_input, bitmap_input);
             }
-            fn [<addassign_boxslice $tl:lower _ boxslice $tr:lower>](output: &mut ColumnWrapper, input: Vec<InputTypes>)
+            fn [<addassign_slice $tl:lower _ slice $tr:lower>](output: &mut ColumnWrapper, input: Vec<InputTypes>)
             {
-                addassign_c::<Box<[$tl]>, Box<[$tr]>, $tl, $tr>(output, input);
+                //Slice Slice
+                type V1=[$tl];
+                type V2=[$tr];
+                let (data_output, index_output, bitmap_output) = output.slice_all_mut::<V1>();
+                let (data_input, index_input, bitmap_input) = match &input[0] {
+                    InputTypes::Ref(a) => (
+                        a.downcast_slice_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                    InputTypes::Owned(a) => (
+                        a.downcast_slice_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                };
+                addassign_c(data_output, index_output,bitmap_output, data_input, index_input, bitmap_input);
+            }
+            fn [<addassign_vec $tl:lower _ slice $tr:lower>](output: &mut ColumnWrapper, input: Vec<InputTypes>)
+            {
+                //Vec Slice
+                type V1=Vec<$tl>;
+                type V2=[$tr];
+
+                let (data_output, index_output, bitmap_output) = output.all_mut::<V1>();
+
+                let (data_input, index_input, bitmap_input) = match &input[0] {
+                    InputTypes::Ref(a) => (
+                        a.downcast_slice_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                    InputTypes::Owned(a) => (
+                        a.downcast_slice_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                };
+                addassign_c(data_output, index_output,bitmap_output, data_input, index_input, bitmap_input);
+            }
+            fn [<addassign_slice $tl:lower _ vec $tr:lower>](output: &mut ColumnWrapper, input: Vec<InputTypes>)
+            {
+                //Slice Vec
+                type V1=[$tl];
+                type V2=Vec<$tr>;
+                let (data_output, index_output, bitmap_output) = output.slice_all_mut::<V1>();
+                let (data_input, index_input, bitmap_input) = match &input[0] {
+                    InputTypes::Ref(a) => (
+                        a.downcast_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                    InputTypes::Owned(a) => (
+                        a.downcast_ref::<V2>(),
+                        a.index(),
+                        a.bitmap(),
+                    ),
+                };
+                addassign_c(data_output, index_output,bitmap_output, data_input, index_input, bitmap_input);
             }
         }
     )+)
@@ -51,14 +157,18 @@ operation_impl! {
 
 pub(crate) fn load_op_dict(dict: &mut OpDictionary) {
     operation_load! {dict;
-            (u64, u64) (u64, u32) (u64, u16) (u64, u8) (u64, bool) (u32,u32)
+        (u64, u64) (u64, u32) (u64, u16) (u64, u8) (u64, bool) (u32,u32)
     };
 }
 
-fn addassign_c<V1, V2, T1, T2>(output: &mut ColumnWrapper, input: Vec<InputTypes>)
-where
-    V1: DerefMut<Target = [T1]> + 'static,
-    V2: Deref<Target = [T2]> + 'static,
+fn addassign_c<T1, T2>(
+    data_output: &mut [T1],
+    index_output: &mut ColumnIndex,
+    bitmap_output: &mut Option<Bitmap>,
+    data_input: &[T2],
+    index_input: &ColumnIndex,
+    bitmap_input: &Option<Bitmap>,
+) where
     T1: AddAssign,
     T1: From<T2>,
     T1: Default,
@@ -69,22 +179,9 @@ where
     //right[0]-->input
     //if right[0] and right[1]-> input_lhs, input_rhs
 
-    let (data_output, index_output, bitmap_output) = output.all_mut::<V1>();
-
-    let (data_input, index_input, bitmap_input) = match &input[0] {
-        InputTypes::Ref(a) => (
-            a.downcast_ref::<V2>(),
-            a.index().as_ref(),
-            a.bitmap().as_ref(),
-        ),
-        InputTypes::Owned(a) => (
-            a.downcast_ref::<V2>(),
-            a.index().as_ref(),
-            a.bitmap().as_ref(),
-        ),
-    };
-
     //The output column should have no index
+    //Otherwise an element of the output vector can be updated twice if
+    //it is indexed twice in the index vector
     assert_eq!(index_output, &None);
 
     let len_output = data_output.len();
@@ -94,6 +191,7 @@ where
         data_input.len()
     };
 
+    //The input and output columns should have the same length
     assert_eq!(len_output, len_input);
 
     match (&index_input, &bitmap_output, &bitmap_input) {
